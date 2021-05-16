675c3167c0db9c04e24bc9963a972a926b23089f
Ramesh Sannareddy <rsannareddy@gmail.com>
2021-05-16 01:49:00 +0000

    Successfully run everything

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
 
-    var interest = principal * years * rate / 100;
+    if (principal <= 0) {
+        alert("Enter a positive number");
+    } else {
 
-    var year = new Date().getFullYear()+parseInt(years);
+        var interest = principal * years * rate / 100;
 
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+        var year = new Date().getFullYear()+parseInt(years);
+        console.log(years + ' ' +  year)
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    }
 }
 
 function updateRate()
